692edd340c5de10a7d60015391327904b37a078e
emalamela <lamelaemanuel@gmail.com>
2017-10-14 17:29:46 -0300

    Ejercicio 3, Ejercicio 4 parcialmente

--- tp2/esqueleto.pl
+++ tp2/esqueleto.pl
@@ -23,25 +23,50 @@ adyacenteEnRango(T,F1,C1,F2,C2) :- adyacente(F1,C1,F2,C2), enRango(T,F2,C2).
 
 %------------------Predicados a definir:------------------%
 
-%   Ejercicio 1   %
+%%% Ejercicio 1 %%%
 
 %contenido(+?Tablero, ?Fila, ?Columna, ?Contenido)
 contenido(Tablero, Fila, Columna, Contenido) :- nth1(Fila, Tablero, F), nth1(Columna, F, Contenido).
 
-%   Ejercicio 2   %
-
-%adyacentes(+Tablero, +Fila, +Columna, -Adyacentes)
-adyacentes(Tablero, Fila, Columna, Adyacentes) :- member([(FAdy, CAdy)], Adyacentes), adyacenteEnRango(Tablero, Fila, Columna, FAdy, CAdy).
-
-%adyacentesLibres(+Tablero, +Fila, +Columna)
-adyacentesLibres(Tablero, Fila, Columna) :- adyacenteEnRango(Tablero, Fila, Columna, FAdy, CAdy), contenido(Tablero, FAdy, CAdy, Ady), Ady \== o.
+%%% Ejercicio 2 %%%
 
 %disponible(+Tablero, ?Fila, ?Columna)
-disponible(Tablero, Fila, Columna) :- Cont \== o, contenido(Tablero, Fila, Columna, Cont), adyacentesLibres(Tablero, Fila, Columna).
+disponible(Tablero, Fila, Columna) :- 
+        posicionLibre(Tablero, Fila, Columna), 
+        forall(adyacenteEnRango(Tablero, Fila, Columna, FilaAdy, ColumnaAdy), posicionLibre(Tablero, FilaAdy, ColumnaAdy)).
+
+%posicionLibre(+?Tablero, ?Fila, ?Columna)
+posicionLibre(Tablero, Fila, Columna) :- contenido(Tablero, Fila, Columna, Cont), Cont \== o.
+
+%%% Ejercicio 3 %%%
 
 %puedoColocar(+CantPiezas, ?Direccion, +Tablero, ?Fila, ?Columna)
+puedoColocar(1, _, Tablero, Fila, Columna) :- disponible(Tablero, Fila, Columna).
+puedoColocar(CantPiezas, Direccion, Tablero, Fila, Columna) :- 
+        CantPiezas > 1, disponible(Tablero, Fila, Columna), siguientePosicionEnDireccion(Direccion, Fila, Columna, X, Y),
+        PiezasRestantes is CantPiezas - 1, puedoColocar(PiezasRestantes, Direccion, Tablero, X, Y).
+
+%siguientePosicionEnDireccion(?Direccion, +Fila, +Columna, -SigFila, -SigColumna)
+siguientePosicionEnDireccion(horizontal, Fila, Columna, SigFila, SigColumna) :- SigFila is Fila, SigColumna is Columna + 1.
+siguientePosicionEnDireccion(vertical, Fila, Columna, SigFila, SigColumna) :- SigFila is Fila + 1, SigColumna is Columna.
+
+%%% Ejercicio 4 %%%
 
 %ubicarBarcos(+Barcos, +?Tablero)
+ubicarBarcos([], _).
+ubicarBarcos([Barco | Barcos], Tablero) :- colocarBarco(Barco, Tablero), ubicarBarcos(Barcos, Tablero).
+
+%colocarBarco(+Barco, +?Tablero)
+colocarBarco(Barco, Tablero) :-
+        setof((X, Y), puedoColocar(Barco, _, Tablero, X, Y), PosicionesDisponibles),
+        member((Fila, Columna), PosicionesDisponibles), colocarBarcoEnDireccion(Barco, _, Tablero, Fila, Columna).
+
+%colocarBarcoEnDirecion(+PiezasBarco, ?Direccion, +?Tablero, +Fila, +Columna)
+colocarBarcoEnDireccion(1, _, Tablero, Fila, Columna) :- contenido(Tablero, Fila, Columna, o).
+colocarBarcoEnDireccion(PiezasBarco, Direccion, Tablero, Fila, Columna) :-
+        PiezasBarco > 1, contenido(Tablero, Fila, Columna, o),
+        siguientePosicionEnDireccion(Direccion, Fila, Columna, SigFila, SigColumna), PiezasRestantes is PiezasBarco - 1,
+        colocarBarcoEnDireccion(PiezasRestantes, Direccion, Tablero, SigFila, SigColumna).
 
 %completarConAgua(+?Tablero)
 
